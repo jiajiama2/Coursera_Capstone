6298cbf28b2cbaad423acebc17752f0fe26cc0ae
jiajiama2 <53720386+jiajiama2@users.noreply.github.com>
2019-08-10 19:00:45 -0400

    #1 Hello

---
+++ Untitled.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be mainly used for the capstone project.
+# IBM Data Science Professional Certificate - Course 9
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+print('Hello Capstone Project Course!')
+
